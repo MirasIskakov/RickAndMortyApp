331e5863bf4f659fa95265de84ca1676d2c7752c
Miras <iskakov91633@gmail.com>
2024-01-29 16:58:22 +0600

    Deleted main storyboard

---
+++ RickAndMorty/RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Miras Iskakov on 29.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
